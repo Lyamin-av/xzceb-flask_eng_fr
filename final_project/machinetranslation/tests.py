93b6adfa71e017189151d2c74e00ada1a75e6c88
Lyamin-av <a.v.lyamin@gmail.com>
2022-01-29 21:35:27 +0000

    task2

---
+++ final_project/machinetranslation/tests.py
@@ -0,0 +1,19 @@
+import unittest
+
+from translator import englishToFrench, frenchToEnglish
+
+class TestForNull(unittest.TestCase):
+    def test1(self):
+        self.assertIsNotNone(englishToFrench)
+    def test2(self):
+        self.assertIsNotNone(frenchToEnglish)    
+
+class TestEnglishToFrench(unittest.TestCase):
+    def test1(self):
+        self.assertEqual(englishToFrench('Hello'),  'Bonjour')
+
+class TestFrenchToEnglish(unittest.TestCase):
+    def test1(self):
+        self.assertEqual(frenchToEnglish('Bonjour'),  'Hello')
+
+unittest.main()
--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -11,8 +11,23 @@ url = os.environ['url']
 
 authenticator = IAMAuthenticator('apikey')
 language_translator = LanguageTranslatorV3(
-    version='28.01.2022',
+    version='2022-01-28',
     authenticator=authenticator
 )
 
 language_translator.set_service_url('url')
+
+
+def englishToFrench(englishText):
+    #write the code here
+    frenchText = language_translator.translate(
+    text='englishText',
+    model_id='en-fr').get_result()
+    return frenchText
+
+def frenchToEnglish(frenchText):
+    #write the code here
+    englishText = language_translator.translate(
+    text='frenchText',
+    model_id='fr-en').get_result()
+    return englishText
