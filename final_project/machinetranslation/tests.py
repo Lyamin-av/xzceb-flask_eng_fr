154169a0d78939b723b8a7b953ae61a124c2b224
lyamin-av <a.v.lyamin@gmail.com>
2022-01-30 20:28:04 +0000

    Task 4

--- final_project/machinetranslation/tests.py
+++ final_project/machinetranslation/tests.py
@@ -10,10 +10,10 @@ class TestForNull(unittest.TestCase):
 
 class TestEnglishToFrench(unittest.TestCase):
     def test1(self):
-        self.assertEqual(english_to_french('Hello'),  'Bonjour')
+        self.assertEqual(english_to_french('Hello'),'Bonjour')
 
 class TestFrenchToEnglish(unittest.TestCase):
     def test1(self):
-        self.assertEqual(french_to_english('Bonjour'),  'Hello')
+        self.assertEqual(french_to_english('Bonjour'),'Hello')
 
 unittest.main()
--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -7,28 +7,29 @@ from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 from dotenv import load_dotenv
 
 load_dotenv()
-
 apikey = os.environ['apikey']
 url = os.environ['url']
 
-authenticator = IAMAuthenticator('apikey')
+authenticator = IAMAuthenticator('0kQedKP98q7hjsoQZP0GQY5dtYwr1UGMRK1gT_P4nxY0')
 language_translator = LanguageTranslatorV3(
     version='2022-01-28',
     authenticator=authenticator
 )
 
-language_translator.set_service_url('url')
+language_translator.set_service_url('https://api.eu-gb.language-translator.watson.cloud.ibm.com/instances/0fcd4c0c-660f-4357-82c4-75bd9e407565')
 
 def english_to_french(englishtext):
     """English to French"""
     frenchtext = language_translator.translate(
-    text='englishtext',
+    text=englishtext,
     model_id='en-fr').get_result()
-    return frenchtext
+    frenchtextfin = frenchtext['translations'][0]['translation']
+    return frenchtextfin
 
 def french_to_english(frenchtext):
     """French to English"""
     englishtext = language_translator.translate(
-    text='frenchtext',
+    text=frenchtext,
     model_id='fr-en').get_result()
-    return englishtext
+    englishtextfin = englishtext['translations'][0]['translation']
+    return englishtextfin
